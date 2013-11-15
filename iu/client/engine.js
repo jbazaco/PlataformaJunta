6cf68269b2adf4d62ca927999194bf8f1a685102
jbazaco <bazaco15@gmail.com>
2013-11-15 18:08:55 +0100

    añadido objeto Game y realizados los cambios necesarios para adaptarlo

--- iu/client/engine.js
+++ iu/client/engine.js
@@ -1,37 +1,73 @@
+Game = new function() {																  
+
+	// Inicializa el juego
+	this.initialize = function(canvasElementId,sprite_data,callback) {
+		this.canvas = document.getElementById(canvasElementId)
+		this.width = this.canvas.width;
+		this.height= this.canvas.height;
+
+		this.ctx = this.canvas.getContext && this.canvas.getContext('2d');
+		if(!this.ctx) { return alert("Please upgrade your browser to play"); }
+
+		this.loop(); 
+
+		SpriteSheet.load (sprite_data,callback);
+	};
+
+	// Bucle del juego
+	var boards = [];
+
+	 this.loop = function() { 
+
+		for(var i=0,len = boards.length;i<len;i++) {
+			if(boards[i]) { 
+				boards[i].draw(Game.ctx);
+			}
+		}
+
+		// Ejecutar dentro de 30 ms
+		setTimeout(Game.loop,30);
+	};
+	// Para cambiar el panel activo en el juego.
+	// Son capas: se dibujan de menor num a mayor
+	// Cada capa tiene que tener en su interfaz draw()
+	this.setBoard = function(num,board) { boards[num] = board; };
+};
+
 SpriteSheet = new function() {
 
-    // Almacena nombre_de_sprite: rectángulo para que sea mas facil
-    // gestionar los sprites del fichero images/sprite.png
-    this.map = { }; 
+	// Almacena nombre_de_sprite: rectángulo para que sea mas facil
+	// gestionar los sprites del fichero images/sprite.png
+	this.map = { }; 
 
-    // Para cargar hoja de sprites. 
-    //
-    // Parámetros: spriteData: parejas con nombre de sprite, rectángulo
-    // callback: para llamarla cuando se haya cargado la hoja de
-    // sprites
-    this.load = function(spriteData,callback) { 
-	this.map = spriteData;
-	this.image = new Image();
-	this.image.onload = callback;
-	this.image.src = 'fichas_carcassonne.png';
-    };
+	// Para cargar hoja de sprites. 
+	//
+	// Parámetros: spriteData: parejas con nombre de sprite, rectángulo
+	// callback: para llamarla cuando se haya cargado la hoja de
+	// sprites
+	this.load = function(spriteData,callback) { 
+		this.map = spriteData;
+		this.image = new Image();
+		this.image.onload = callback;
+		this.image.src = 'fichas_carcassonne.png';
+	};
 
  
-    // Para dibujar sprites individuales en el contexto de canvas ctx
-    //
-    // Parámetros: contexto, string con nombre de sprite para buscar
-    //  en this.map, x e y en las que dibujarlo, y opcionalmente,
-    //  frame para seleccionar el frame de un sprite que tenga varios
-    //  como la explosion
-    this.draw = function(ctx,sprite,x,y,frame) {
-	var s = this.map[sprite];
-	if(!frame) frame = 0;
-	ctx.drawImage(this.image,
-                      s.sx + frame * s.w, 
-                      s.sy, 
-                      s.w, s.h, 
-                      Math.floor(x), Math.floor(y),
-                      s.w, s.h);
-    };
+	// Para dibujar sprites individuales en el contexto de canvas ctx
+	//
+	// Parámetros: contexto, string con nombre de sprite para buscar
+	//  en this.map, x e y en las que dibujarlo, y opcionalmente,
+	//  frame para seleccionar el frame de un sprite que tenga varios
+	//  como la explosion
+	this.draw = function(ctx,sprite,x,y,frame) {
+		var s = this.map[sprite];
+		if(!frame) frame = 0;
+		ctx.drawImage(this.image,
+					  s.sx + frame * s.w, 
+					  s.sy, 
+					  s.w, s.h, 
+					  Math.floor(x), Math.floor(y),
+					  s.w, s.h);
+	};
 }
 
--- iu/client/game.js
+++ iu/client/game.js
@@ -1,70 +1,62 @@
-function startGame() {
-    
-   
-    var canvas = $('#tablero')[0];
+var sprites = {
+	m: { sx: 5, sy: 6, w: 108, h: 108},					//monasterio
+	mc: { sx: 130, sy: 6, w: 108, h: 108},				//monasterio con camino
+	cr: { sx: 255, sy: 5, w: 108, h: 108},				//camino recto
+	cc: { sx: 384, sy: 6, w: 108, h: 108},				//camino curva
+	c3: { sx: 4, sy: 151, w: 108, h: 108},				//cruce de 3 caminos
+	c4: { sx: 127, sy: 154, w: 108, h: 108},			//cruce de 4 caminos
+	cmur: { sx: 255, sy: 153, w: 108, h: 108},			//camino recto con muralla al lado(una de las fichas es la inicial)
+	ccmur: { sx: 381, sy: 151, w: 108, h: 108},			//camino con curva y con muralla al lado
+	chmur: { sx: 3, sy: 298, w: 108, h: 108},			//camino hacia muralla
+	c3mur: { sx: 127, sy: 298, w: 108, h: 108},			//cruce de 3 caminos con muralla al lado
+	ccmur2: { sx: 255, sy: 297, w: 108, h: 108},		//camino con curva con 2 lados de ciudad
+	ccmur3: { sx: 380, sy: 295, w: 108, h: 108},		//camino con curva y muralla al lado(otro)
+	murcam: { sx: 6, sy: 445, w: 108, h: 108},			//media ficha muralla media ficha campo
+	mur2: { sx: 126, sy: 445, w: 108, h: 108},			//una muralla a cada lado de la ficha
+	mur2c: { sx: 255, sy: 445, w: 108, h: 108},			//2 murallas en lados contiguos
+	mur1: { sx: 379, sy: 444, w: 108, h: 108},			//1 muralla en un lado y el resto campo
+	ciudad: { sx: 4, sy: 587, w: 108, h: 108},			//todo ciudad
+	ciucam: { sx: 135, sy: 588, w: 108, h: 108},		//ciudad con un lado de campo
+	ciucam2: { sx: 253, sy: 586, w: 108, h: 108}		//ciudad con 2 lados opuestos de campo
+};
 
-    
-    var ctx = canvas.getContext && canvas.getContext('2d');
 
-    if(!ctx) {
-	// Si no hay contexto 2d informamos al usuario
-	alert('Please upgrade your browser');
-	return;
-    }
-    
-    // Dibujar rectangulo azul
-    ctx.fillStyle = "#44cbff";
-    ctx.fillRect(50,50,850,650);
-
-    ctx.fillStyle = "#c9c9c9";
-    ctx.fillRect(900,50,20,650);
-
-    ctx.fillStyle = "#000000";
-    ctx.fillRect(920,50,200,650);
-    
-     SpriteSheet.load({
-		m: { sx: 5, sy: 6, w: 108, h: 108},				//monasterio
-        mc: { sx: 130, sy: 6, w: 108, h: 108},			//monasterio con camino
-        cr: { sx: 255, sy: 5, w: 108, h: 108},			//camino recto
-        cc: { sx: 384, sy: 6, w: 108, h: 108},			//camino curva
-        c3: { sx: 4, sy: 151, w: 108, h: 108},			//cruce de 3 caminos
-        c4: { sx: 127, sy: 154, w: 108, h: 108},		//cruce de 4 caminos
-		cmur: { sx: 255, sy: 153, w: 108, h: 108},		//camino recto con muralla al lado(una de las fichas es la inicial)
-		ccmur: { sx: 381, sy: 151, w: 108, h: 108},		//camino con curva y con muralla al lado
-		chmur: { sx: 3, sy: 298, w: 108, h: 108},		//camino hacia muralla
-		c3mur: { sx: 127, sy: 298, w: 108, h: 108},		//cruce de 3 caminos con muralla al lado
-		ccmur2: { sx: 255, sy: 297, w: 108, h: 108},	//camino con curva con 2 lados de ciudad
-		ccmur3: { sx: 380, sy: 295, w: 108, h: 108},	//camino con curva y muralla al lado(otro)
-		murcam: { sx: 6, sy: 445, w: 108, h: 108},		//media ficha muralla media ficha campo
-		mur2: { sx: 126, sy: 445, w: 108, h: 108},		//una muralla a cada lado de la ficha
-		mur2c: { sx: 255, sy: 445, w: 108, h: 108},		//2 murallas en lados contiguos
-		mur1: { sx: 379, sy: 444, w: 108, h: 108},		//1 muralla en un lado y el resto campo
-		ciudad: { sx: 4, sy: 587, w: 108, h: 108},		//todo ciudad
-		ciucam: { sx: 135, sy: 588, w: 108, h: 108},	//ciudad con un lado de campo
-		ciucam2: { sx: 253, sy: 586, w: 108, h: 108}	//ciudad con 2 lados opuestos de campo
-		
-    }, function() {
-	SpriteSheet.draw(ctx,"m",50,50);
-	SpriteSheet.draw(ctx,"mc",170,50);
-	SpriteSheet.draw(ctx,"cr",300,50);
-	SpriteSheet.draw(ctx,"cc",420,50);
-	SpriteSheet.draw(ctx,"c3",540,50);
-	SpriteSheet.draw(ctx,"c4",660,50);
-	SpriteSheet.draw(ctx,"cmur",780,50);
-	SpriteSheet.draw(ctx,"ccmur",50,170);
-	SpriteSheet.draw(ctx,"chmur",170,170);
-	SpriteSheet.draw(ctx,"c3mur",300,170);
-	SpriteSheet.draw(ctx,"ccmur2",420,170);
-	SpriteSheet.draw(ctx,"ccmur3",540,170);
-	SpriteSheet.draw(ctx,"murcam",660,170);
-	SpriteSheet.draw(ctx,"mur2",780,170);
-	SpriteSheet.draw(ctx,"mur2c",50,290);
-	SpriteSheet.draw(ctx,"mur1",170,290);
-	SpriteSheet.draw(ctx,"ciudad",300,290);
-	SpriteSheet.draw(ctx,"ciucam",420,290);
-	SpriteSheet.draw(ctx,"ciucam2",540,290);
-
+startGame = function() {
 	
-    });
+	// Dibujar rectangulo azul
+	Game.ctx.fillStyle = "#44cbff";
+	Game.ctx.fillRect(50,50,850,650);
+
+	//Dibujar barra separadora
+	Game.ctx.fillStyle = "#c9c9c9";
+	Game.ctx.fillRect(900,50,20,650);
+
+	//Dibujar barra-menu
+	Game.ctx.fillStyle = "#000000";
+	Game.ctx.fillRect(920,50,200,650); 
+
+	//Quitar cuando avancemos
+	SpriteSheet.draw(Game.ctx,"m",50,50);
+	SpriteSheet.draw(Game.ctx,"mc",170,50);
+	SpriteSheet.draw(Game.ctx,"cr",300,50);
+	SpriteSheet.draw(Game.ctx,"cc",420,50);
+	SpriteSheet.draw(Game.ctx,"c3",540,50);
+	SpriteSheet.draw(Game.ctx,"c4",660,50);
+	SpriteSheet.draw(Game.ctx,"cmur",780,50);
+	SpriteSheet.draw(Game.ctx,"ccmur",50,170);
+	SpriteSheet.draw(Game.ctx,"chmur",170,170);
+	SpriteSheet.draw(Game.ctx,"c3mur",300,170);
+	SpriteSheet.draw(Game.ctx,"ccmur2",420,170);
+	SpriteSheet.draw(Game.ctx,"ccmur3",540,170);
+	SpriteSheet.draw(Game.ctx,"murcam",660,170);
+	SpriteSheet.draw(Game.ctx,"mur2",780,170);
+	SpriteSheet.draw(Game.ctx,"mur2c",50,290);
+	SpriteSheet.draw(Game.ctx,"mur1",170,290);
+	SpriteSheet.draw(Game.ctx,"ciudad",300,290);
+	SpriteSheet.draw(Game.ctx,"ciucam",420,290);
+	SpriteSheet.draw(Game.ctx,"ciucam2",540,290);
 }
-$(startGame);
+
+$(function() {
+    Game.initialize("tablero",sprites,startGame);
+});
