7a8b0390f4e713085014b38adbff9a8235d8a220
jotacv <jotacv@hotmail.es>
2013-11-19 10:19:53 +0100

    Añadidos los ficheros del chatalien. No esta añadido meteor.

---
+++ client/client.js
@@ -0,0 +1,47 @@
+Meteor.subscribe("msgs");
+
+var Clip = function(msg,maxlen){
+	if(msg.length>maxlen){
+		var msgaux="";
+		for(var i = 1;i<Math.floor(msg.length/maxlen);i++){
+			msgaux+=msg.substring(maxlen*i,maxlen*(i+1))+"\n";
+		}
+		return msgaux;
+	}else{
+		return msg
+	}
+};
+
+Template.input.events={
+	'keydown input#message': function(event){
+		if (event.which==13){
+			var message=$("#message");
+			if (message.val()!=""){
+				var msg=Clip(message.val(),50);
+				if (Meteor.user()){
+					var name = Meteor.user().username;
+				}else{
+					var name="Anon";
+				}
+				Messages.insert({
+					name:name,
+					message:message.val(),
+					time:Date.now()
+				});
+			}
+			message.val("");		
+		}
+	}
+}
+
+Accounts.ui.config({
+	passwordSignupFields:"USERNAME_AND_OPTIONAL_EMAIL"
+});
+
+Deps.autorun(function(){
+	var chatArea = $('#firstRow');
+	var msgs = Messages.find({},{sort:{time:-1}, limit:1});	
+	msgs.forEach(function(message){
+		chatArea.prepend("<tr><td><strong>"+message['name']+"</strong>:</td><td><div>"+message['message']+"</div></td>");
+	});
+})
