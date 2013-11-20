ef49f2001da57bef560b92dae365cc25cf54a4d5
Alejandro Campos Sanchez <alexbetis92@gmail.com>
2013-11-20 13:47:14 +0100

    bucle para poder arrastrar cualquier elemento que pertenezca a boards

--- iu/client/game.js
+++ iu/client/game.js
@@ -129,11 +129,12 @@ elemInPos = function(x, y) {
 		alert('No es tu turno');
 		return null;
 	}
-
+	for(var i=1,len = boards.length;i<len;i++) {
 	//Aqui las acciones que necesiten estar en tu turno
-	if (y > FichaActual.y && y < FichaActual.y+FichaActual.h 
-				&& x > FichaActual.x && x < FichaActual.x+FichaActual.w) {
-		return FichaActual;
+		if (y > boards[i].y && y < boards[i].y+boards[i].h 
+				&& x > boards[i].x && x < boards[i].x+boards[i].w) {
+			return boards[i];
+		}
 	}
 	return null;
 }
