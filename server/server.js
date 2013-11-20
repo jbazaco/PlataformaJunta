518c7e9395f4419019bffb9c23d4df677449f6d7
jotacv <jotacv@hotmail.es>
2013-11-20 21:22:09 +0100

    Añadido metodos a Meteor.methods(). En Server: 	Pues eso, y comentarios a los metodos.
    ::POR HACER::
    Que alguien lo revise, le de el visto bueno y mezclar la rama.

--- server/server.js
+++ server/server.js
@@ -6,66 +6,73 @@ Meteor.publish('partidas',function(){
 	return Partidas.find({});
 });
 
+Meteor.methods({
+	//  Cada vez que se quiera almacenar un movimiento de una partida se llamará 
+	//  a esta funcion. Se comprueba que se el judgador que inicia el momimiento
+	//  está autorizado (está en la lista de jugadores), despues se almacena la 
+	//  jugada en la lista de jugadas de la partida.
+	RegistrarMovimiento : function(id_partida,jugador,movimiento){
+		console.log("Registrar Movimientos");
+		console.log(this.args);
+		var id = Partidas.findOne({id:id_partida})._id;
+		//if(jugadorpermitido)
+		Partidas.update(id,{$push:{jugadas:movimiento}});
+	},
+
+	// Esta función devuelve el ultimo movimiento jugado en la partida
+	// AUN NO ESTA CLARO si cualquiera puede mirar el ultimo movimiento
+	// o debe estar en alguna de las listas de jugadores (usuarios, 
+	// jugadores de partida o invitados a partida).
+	UltimoMovimiento : function(id_partida){
+		console.log("Ultimo Movimiento");
+		console.log(this.args);
+		//if(jugadorpermitido)?
+		var jugadas = Partidas.findOne({id:id_partida}).jugadas;
+		return jugadas[jugadas.length-1];
+	},
 
 
-//  Cada vez que se quiera almacenar un movimiento de una partida se llamará 
-//  a esta funcion. Se comprueba que se el judgador que inicia el momimiento
-//  está autorizado (está en la lista de jugadores), despues se almacena la 
-//  jugada en la lista de jugadas de la partida.
-RegistrarMovimiento = function(id_partida,jugador,movimiento){
-    console.log("Registrar Movimientos");
-    console.log(this.args);
-	var id = Partidas.findOne({id:id_partida})._id;
-	//if(jugadorpermitido)
-	Partidas.update(id,{$push:{jugadas:movimiento}});
-};
-
-// Esta función devuelve el ultimo movimiento jugado en la partida
-// AUN NO ESTA CLARO si cualquiera puede mirar el ultimo movimiento
-// o debe estar en alguna de las listas de jugadores (usuarios, 
-// jugadores de partida o invitados a partida).
-UltimoMovimiento = function(id_partida){
-    console.log("Ultimo Movimiento");
-    console.log(this.args);
-	//if(jugadorpermitido)?
-	var jugadas = Partidas.findOne({id:id_partida}).jugadas;
-	return jugadas[jugadas.length-1];
-};
+	// Devuelve el jugador AL QUE LE TOCA dando un identificador de partida
+	// Si es el primer turno devuelve un jugador aleatorio de la lista de jugadores.
+	// El jugador al que le toca es EL SIGUIENTE al que ha jugado la ultima jugada,
+	// en la lista de jugadores según el orden en el que están almacenados en el array
+	// de jugadores
+	VerTurno : function(id_partida){
+		console.log("VerTurno");
+		console.log(this.args);
+	// 	if(jugadorpermitido)?
+		var partida = Partidas.findOne({id:id_partida}).jugadas;
+		if (partida.jugadas.length()){
+			return partida.jugadores[(partida.jugadores.indexOf(partida.jugadas[jugadas.length-1].jugador)+1)%partida.jugadores.length];
+		}else{
+			return(partida.jugadores[Math.floor(Math.random()*partida.jugadores.length)]);
+		}
+	},
 
 
-//Devuelve el jugador AL QUE LE TOCA dando un identificador de partida
-// Si es el primer turno devuelve un jugador aleatorio de la lista de jugadores.
-// El jugador al que le toca es EL SIGUIENTE al que ha jugado la ultima jugada.
-VerTurno = function(id_partida){
-	console.log("VerTurno");
-	console.log(this.args);
-// 	if(jugadorpermitido)?
-	var partida = Partidas.findOne({id:id_partida}).jugadas;
-	if (partida.jugadas.length()){
-		return partida.jugadores[(partida.jugadores.indexOf(partida.jugadas[jugadas.length-1].jugador)+1)%partida.jugadores.length];
-	}else{
-		return(partida.jugadores[Math.floor(Math.random()*partida.jugadores.length)]);
+	// Mete una nueva partida en el servidor. Devuelve un identificador
+	// de partida UNICO no coincidente con el idenificador foráneo al que 
+	// el cliente debe suscribirse en su Deps.autorun().
+	// Jugadores es un array con el identificador de cada jugador (nombre?)
+	// Opciones es un map con las opciones que se quieran pasar a la partida.
+	// Invitados es un array con los jugadores que observan la partida.
+	SuscribirPartida : function(jugadores,opciones,invitados){
+		console.log("SubscribirPartida");
+		console.log(this.args);
+	// 	if(permitido)?
+		var id =GetSeq();
+		Partidas.insert({
+			id:id,
+			jugadores: jugadores,
+			invitados: [],
+			opciones: op,
+			jugadas:[]
+		})
+		Meteor.publish('Partida'+id.toString(),function(){
+			return Partidas.find({id:id},{jugadores:1,invitados:1,opciones:1,jugadas:1});
+		})
 	}
-};
-
-
-// Mete una nueva partida en el servidor. Devuelve un identificador
-// de partida UNICO no coincidente con el idenificador foráneo.
-// Jugadores es un array con el identificador de cada jugador (nombre?)
-// Opciones es un map con las opciones que se quieran pasar a la partida.
-// Invitados es un array con los jugadores que observan la partida.
-SubscribirPartida = function(jugadores,opciones,invitados){
-	console.log("SubscribirPartida");
-	console.log(this.args);
-// 	if(permitido)?
-	Partidas.insert({
-		id:GetSeq(),
-		jugadores: jugadores,
-		invitados: [],
-		opciones: op,
-		jugadas:[]
-	})
-};
+})
 
 
 var GetSeq = function(){
