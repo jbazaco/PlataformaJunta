bff9701fdf9af753d93da9fb151575a2777d8ed5
Israel Bejarano Alvarez <israel.bejarano.a@gmail.com>
2014-01-27 17:09:09 +0100

    ColocarSeguidorCastillo: Función recursiva y casos iniciales, primer algoritmo. Hay que seguir puliendo.

--- server/compatibility/seguidor.js
+++ server/compatibility/seguidor.js
@@ -87,5 +87,129 @@ ColocarSeguidorCastillo = function(Id, Ficha, PosSeg, X, Y){
 			return true;
 	};
 	
+	RecursivaSeguidor= function(Ficha, Prohibido,X,Y){
+		console.log("                                ");
+		if (Tablero[X][Y]!=0){
+			console.log("LA FICHA: " + Ficha.nombre + " Coordenadas: X= " + X + "||| Y= " + Y);
+			//Points = true;
+			if (fichasLadoCastilloConexos.indexOf(Ficha.nombre)!=-1){	//si la ficha esta en este array
+				console.log("la ficha "+ Ficha.nombre + " está en el array conexo | CX: " + X + "||| CY: " + Y);
+				console.log("DarDirec es: " + DarDirec(X,Y));
+				if (DarDirec(X,Y)){
+					if ((Ficha.u == "castillo") && (Prohibido != "arriba")){
+						Y1=Y-1;						
+						MeteDirec(X,Y);
+						
+						RecursivaSeguidor(Tablero[X][Y1],"abajo",X,Y1);
+						//Points = false;
+					}		
+					if ((Ficha.r=="castillo") && (Prohibido!= "derecha")){
+						//if (Points){						
+							//MeteDirec(X,Y);
+							
+						//}
+						X1=X+1;
+						RecursivaSeguidor(Tablero[X1][Y],"izquierda",X1,Y);
+						//Points = false;
+					}
+					if ((Ficha.d=="castillo") && (Prohibido!= "abajo")){
+						//if (Points){						
+							//MeteDirec(X,Y);
+							
+						//}
+						Y2=Y+1;
+						RecursivaSeguidor(Tablero[X][Y2],"arriba",X,Y2);
+						//Points = false;
+					}
+					if ((Ficha.l=="castillo") && (Prohibido!= "izquierda")){
+						//if (Points){					
+							//MeteDirec(X,Y);
+							
+						//}
+						X2=X-1;
+						RecursivaSeguidor(Tablero[X2][Y],"derecha",X2,Y);
+						//Points = false;
+					}
+				}
+			}
+			else if (DarDirec(X,Y)){
+				console.log("la ficha "+ Ficha.nombre + " está en el array inconexo.");
+				
+				MeteDirec(X,Y);				
+				//console.log("los puntos intermedios en la ficha inconexa " + Ficha.nombre + " son: " + puntos);
+			}
+		}
+		else{
+			console.log("El tablero está vacío.");
+			//puntos=0;
+		}
+	};
+	
+	//tratamos el caso de que llega una ficha con seguidor
+	//entramos en el caso de las fichas inconexas
+	if (fichas2LadosCierranCastillo.indexOf(Ficha.nombre)!=-1){
+		console.log("x e y iniciales "+X+"   "+ Y);
+		//tengo que ver las 4 posiciones del seguidor
+		if (PosSeguidor==1){
+			Y1=Y-1;
+			
+			MeteDirec(X,Y);
+			RecursivaSeguidor(Tablero[X][Y1], "abajo", X, Y1);
+		}
+		else if (PosSeguidor==2){
+			//console.log("posicion del seguidor es: " + PosSeguidor);
+			X1=X+1;
+			
+			MeteDirec(X,Y);
+			RecursivaSeguidor(Tablero[X1][Y], "izquierda", X1, Y);
+		}
+		else if (PosSeguidor==3){
+			//console.log("posicion del seguidor es: " + PosSeguidor);
+			Y2=Y+1;
+			
+			MeteDirec(X,Y);
+			RecursivaSeguidor(Tablero[X][Y2], "arriba", X, Y2);
+		}
+		else if (PosSeguidor==4){
+			//console.log("posicion del seguidor es: " + PosSeguidor);
+			X2=X-1;
+			
+			MeteDirec(X,Y);
+			RecursivaSeguidor(Tablero[X2][Y], "derecha", X2, Y);
+		}
+	}
+	else{
+		console.log("La ficha inicial es conexa");
+		MeteDirec(X,Y);
+		//Points = true;
+		if (Ficha.u == "castillo"){
+			console.log("Entra Arriba");
+			Y1=Y-1;
+			
+			RecursivaSeguidor(Tablero[X][Y1],"abajo",X,Y1);
+			//Points = false;
+		}
+		if ((Ficha.r=="castillo")){
+			console.log("Entra Derecha");
+			X1=X+1;
+			
+			RecursivaSeguidor(Tablero[X1][Y],"izquierda",X1,Y);
+			//Points = false;
+		}
+		if ((Ficha.d=="castillo")){
+			console.log("Ficha.nombre: " + Ficha.nombre);
+			console.log("Entra Abajo");
+			Y2=Y+1;			
+				
+			RecursivaSeguidor(Tablero[X][Y2],"arriba",X,Y2);
+			//Points = false;
+		}
+		if ((Ficha.l=="castillo")){
+			console.log("Entra Izquierda");
+			X2=X-1;
+			RecursivaSeguidor(Tablero[X2][Y],"derecha",X2,Y);
+		}
+		
+	}
 	
 };
